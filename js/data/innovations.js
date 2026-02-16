e716dffef610fd3e0ea3c566b4eeb0b54ad95c1d
Sazione <saezjeomari@gmail.com>
2026-02-16 14:35:56 +0800

    ADDED INNOVATION IMAGESSS

---
+++ js/data/innovations.js
@@ -0,0 +1,44 @@
+const INNOVATIONS = [
+  {
+    title: "IronMate",
+    description: "*Description*",
+    image: "assets/images/innovation.png",
+    alt: "Medical Incubator",
+    icon: "🩺" // fallback emoji
+  },
+  {
+    title: "Philippine Eagle Optimization Algorithm",
+    description: "*Description*",
+    image: "assets/images/innovation.png",
+    alt: "Electric Jeepney",
+    icon: "💡"
+  },
+  {
+    title: "TUSLOB",
+    description: "*Description*",
+    image: "assets/images/innovation.png",
+    alt: "SALT Farming",
+    icon: "🌾"
+  },
+  {
+    title: "Bagoong",
+    description: "*Description*",
+    image: "assets/images/innovation.png",
+    alt: "Coconut Biofuel",
+    icon: "🥥"
+  },
+  {
+    title: "ZWPFS",
+    description: "*Description*",
+    image: "assets/images/innovation.png",
+    alt: "Mosquito Trap",
+    icon: "🦟"
+  },
+  {
+    title: "CAMERASOFKSAPIFAS",
+    description: "*Description*",
+    image: "assets/images/innovation.png",
+    alt: "Salt ID System",
+    icon: "📱"
+  }
+];
